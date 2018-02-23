3579c932cd9ac33d3cadfe1d86fe1b93e80f4b32
morgan-team <35870424+morgan-team@users.noreply.github.com>
2018-02-23 14:16:45 +0330

    Update plugins.lua

--- api/plugins/plugins.lua
+++ api/plugins/plugins.lua
@@ -22,7 +22,7 @@ local function plugin_exists( name )
 end
 
 local function list_all_plugins(only_enabled)
-  local tmp = '\n\n[MaTaDoRTeaM](Telegram.Me/MaTaDoRTeaM)'
+  local tmp = '\n\n[morganteam](Telegram.Me/morgan_team)'
   local text = ''
   local nsum = 0
   for k, v in pairs( plugins_names( )) do
@@ -68,7 +68,7 @@ local function list_plugins(only_enabled)
      -- text = text..v..'  '..status..'\n'
     end
   end
-  local text = text.."\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n[MaTaDoRTeaM](Telegram.Me/MaTaDoRTeaM)"
+  local text = text.."\n_🔃All Plugins Reloaded_\n\n"..nact.." *✔️Plugins Enabled*\n"..nsum.." *📂Plugins Installed*\n\n[morgan](Telegram.Me/morgan_team)"
 return text
 end
 
